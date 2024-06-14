6db489087a953e3b9b3112322f286def956c1d42
ezaki <shoken4797@gmail.com>
2024-06-14 10:58:21 +0900

    edit

--- tests/add.test.ts
+++ tests/add.test.ts
@@ -2,11 +2,11 @@ import { add } from "../add";
 
 describe("add", (): void => {
   test("should return 3", (): void => {
-    const result: number = add(1, 2);
+    const result: number = add(1, 3);
     expect(result).toEqual(3);
   });
   test("should return 4", (): void => {
-    const result: number = add(3, 2);
+    const result: number = add(3, 1);
     expect(result).toEqual(4);
   });
 });
