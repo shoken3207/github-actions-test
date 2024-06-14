56a8846b93232f13846fa8d3c12abe0b6ded3b83
ezaki <shoken4797@gmail.com>
2024-06-14 10:17:28 +0900

    add minus method

---
+++ tests/minus.test.ts
@@ -0,0 +1,11 @@
+import { minus } from "../minus";
+describe("minus", (): void => {
+  test("should return 3", (): void => {
+    const result: number = minus(5, 2);
+    expect(result).toEqual(3);
+  });
+  test("should return 1", (): void => {
+    const result: number = minus(4, 2);
+    expect(result).toEqual(1);
+  });
+});
